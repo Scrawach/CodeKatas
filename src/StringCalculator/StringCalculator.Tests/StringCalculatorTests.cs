b83f42e177ce27aa08f34e3980477a00c2c926bc
scrawach <scrawach@gmail.com>
2024-02-12 10:08:06 +0300

    [red] implemented unit testing for adding single number to calculator

--- src/StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
+++ src/StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -16,4 +16,19 @@ public class StringCalculatorTests
         // answer
         result.Should().Be(0);
     }
+
+    [TestCase("1", 1)]
+    [TestCase("2", 2)]
+    [TestCase("3", 3)]
+    public void WhenAddSingleNumber_ThenShouldReturnThisNumber(string input, int expected)
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        var result = calculator.Add(input);
+
+        // answer
+        result.Should().Be(expected);
+    }
 }
