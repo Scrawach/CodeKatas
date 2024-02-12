2bdbac8ae63ed84de7a6f5600ebd778a7f4c3fb9
scrawach <scrawach@gmail.com>
2024-02-12 10:18:39 +0300

    [green] implemented working functionality for handle newline between numbers

--- src/StringCalculator/StringCalculator/StringCalculator.cs
+++ src/StringCalculator/StringCalculator/StringCalculator.cs
@@ -5,7 +5,7 @@ public class StringCalculator
     public int Add(string args) =>
         string.IsNullOrWhiteSpace(args) 
             ? 0 
-            : Add(args.Split(','));
+            : Add(args.Split(',', '\n'));
 
     private static int Add(IEnumerable<string> args) =>
         args.Select(int.Parse).Sum();
