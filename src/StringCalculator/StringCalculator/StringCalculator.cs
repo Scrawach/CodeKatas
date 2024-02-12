722083ac6418ec431a09a8cfcde9767e4c837d5e
scrawach <scrawach@gmail.com>
2024-02-12 10:12:20 +0300

    [green] implemented logic for sum numbers separated by commas

--- src/StringCalculator/StringCalculator/StringCalculator.cs
+++ src/StringCalculator/StringCalculator/StringCalculator.cs
@@ -5,5 +5,8 @@ public class StringCalculator
     public int Add(string args) =>
         string.IsNullOrWhiteSpace(args) 
             ? 0 
-            : int.Parse(args);
+            : Add(args.Split(','));
+
+    public int Add(IEnumerable<string> args) =>
+        args.Select(int.Parse).Sum();
 }
