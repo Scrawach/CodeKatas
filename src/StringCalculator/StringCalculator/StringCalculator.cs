45f6abb3eeb4f863b58255328b66d953d5600273
scrawach <scrawach@gmail.com>
2024-02-12 10:25:13 +0300

    [refactoring] refactoring for string calculator

--- src/StringCalculator/StringCalculator/StringCalculator.cs
+++ src/StringCalculator/StringCalculator/StringCalculator.cs
@@ -7,17 +7,24 @@ public class StringCalculator
             ? 0 
             : ExecuteAdd(args);
 
-    private static int ExecuteAdd(string args)
-    {
-        if (args.StartsWith("//"))
-        {
-            var customDelimiter = args[2];
-            var numbers = args.Split('\n')[1];
-            return Add(numbers.Split(customDelimiter));
-        }
-        return Add(args.Split(',', '\n'));
-    }
-    
-    private static int Add(IEnumerable<string> args) =>
+    private static int ExecuteAdd(string args) =>
+        HasCustomDelimiter(args)
+            ? ExecuteAdd(CustomSplit(args))
+            : ExecuteAdd(DefaultSplit(args));
+
+    private static bool HasCustomDelimiter(string args) =>
+        args.StartsWith("//");
+
+    private static int ExecuteAdd(IEnumerable<string> args) =>
         args.Select(int.Parse).Sum();
+
+    private static IEnumerable<string> CustomSplit(string args)
+    {
+        var customDelimiter = args[2];
+        var numbers = args.Split('\n')[1];
+        return numbers.Split(customDelimiter);
+    }
+
+    private static IEnumerable<string> DefaultSplit(string args) =>
+        args.Split(',', '\n');
 }
