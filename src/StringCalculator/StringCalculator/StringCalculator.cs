c64e5b10e3326d80344ca79e9017de9ec6f778cb
scrawach <scrawach@gmail.com>
2024-02-12 10:22:44 +0300

    [green] implemented custom delimiter logic

--- src/StringCalculator/StringCalculator/StringCalculator.cs
+++ src/StringCalculator/StringCalculator/StringCalculator.cs
@@ -5,8 +5,19 @@ public class StringCalculator
     public int Add(string args) =>
         string.IsNullOrWhiteSpace(args) 
             ? 0 
-            : Add(args.Split(',', '\n'));
+            : ExecuteAdd(args);
 
+    private static int ExecuteAdd(string args)
+    {
+        if (args.StartsWith("//"))
+        {
+            var customDelimiter = args[2];
+            var numbers = args.Split('\n')[1];
+            return Add(numbers.Split(customDelimiter));
+        }
+        return Add(args.Split(',', '\n'));
+    }
+    
     private static int Add(IEnumerable<string> args) =>
         args.Select(int.Parse).Sum();
 }
