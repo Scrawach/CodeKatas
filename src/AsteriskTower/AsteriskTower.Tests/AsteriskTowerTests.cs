2b34a2bb163f4f0b3d032319534e447b6caee77c
scrawach <scrawach@gmail.com>
2024-02-13 14:44:52 +0300

    [red] implemented test for empty array builded tower

--- src/AsteriskTower/AsteriskTower.Tests/AsteriskTowerTests.cs
+++ src/AsteriskTower/AsteriskTower.Tests/AsteriskTowerTests.cs
@@ -1,6 +1,19 @@
+using FluentAssertions;
+
 namespace AsteriskTower.Tests;
 
 public class AsteriskTowerTests
 {
-    
+    [Test]
+    public void WhenAsteriskTower_HasZeroRows_ThenShouldReturnEmptyArray()
+    {
+        // arrange
+        var asteriskTower = new AsteriskTower(0);
+
+        // act
+        var stringTower = asteriskTower.Build();
+
+        // answer
+        stringTower.Should().BeEmpty();
+    }
 }
---
+++ src/AsteriskTower/AsteriskTower/AsteriskTower.cs
@@ -0,0 +1,14 @@
+namespace AsteriskTower;
+
+public class AsteriskTower
+{
+    public AsteriskTower(int rows)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string[] Build()
+    {
+        throw new NotImplementedException();
+    }
+}
