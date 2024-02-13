52a144cbd38cc18684cef496055e4fe0d9219f80
scrawach <scrawach@gmail.com>
2024-02-13 14:47:37 +0300

    [red] added unit test for 1 row

--- src/AsteriskTower/AsteriskTower.Tests/AsteriskTowerTests.cs
+++ src/AsteriskTower/AsteriskTower.Tests/AsteriskTowerTests.cs
@@ -16,4 +16,17 @@ public class AsteriskTowerTests
         // answer
         stringTower.Should().BeEmpty();
     }
+
+    [Test]
+    public void WhenAsteriskTower_HasOneRow_ThenShouldReturnOneSymbol()
+    {
+        // arrange
+        var asteriskTower = new AsteriskTower(1);
+
+        // act
+        var stringTower = asteriskTower.Build();
+
+        // answer
+        stringTower.Should().BeSameAs(new[] { "*"});
+    }
 }
